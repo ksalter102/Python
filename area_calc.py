e5c5f89dee3eaacc33d76bb4fa36b8310a646680
ksalter102 <110357096+ksalter102@users.noreply.github.com>
2022-08-28 14:51:39 +0100

    Add files via upload

---
+++ area_calc.py
@@ -0,0 +1,13 @@
+import math
+
+
+test_h = int(input("Height of the wall:"))
+test_w = int(input("Width of the wall: "))
+coverage = 5
+
+def paint_calc(height, width, cover):
+    area = height * width
+    num_of_cans = math.ceil(area / cover)
+    print(f"You'll need {num_of_cans} cans of paint.")
+
+paint_calc(height=test_h, width=test_w, cover=coverage) #key
